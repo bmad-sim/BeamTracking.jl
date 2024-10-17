5df1c2f7189beaf20c1e9ca64a4d985efc81250b
Documenter.jl <documenter@juliadocs.github.io>
2024-10-17 21:04:29 +0000

    build based on 97d0dea

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#BeamTracking","page":"Home","title":"BeamTracking","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for BeamTracking.","category":"page"},{"location":"","page":"Home","title":"Home","text":"123","category":"page"}]
+}
