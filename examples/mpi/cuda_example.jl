e27834e1ee0857b993cb2ff307cee9cecd200d45
jackelley <jack.kelley1201@gmail.com>
2025-06-02 16:31:04 -0600

    bit beamline error

--- examples/mpi/cuda_example.jl
+++ examples/mpi/cuda_example.jl
@@ -10,12 +10,15 @@ include("../../test/lattices/esr.jl") # Beamline symbol is "ring"
 # Currently only Linear tracking is supported, enable it for each element
 foreach(t -> t.tracking_method = Linear(), ring.line)
 n_particles = 10000000 #parse(Int, ARGS[1])
+bitsring = BitsBeamline(ring)
+CUDA.@profile begin
+    CUDA.@time begin
 
-start_time = time()
-
-v_gpu = CUDA.zeros(Float64, n_particles, 6) 
-bunch = Bunch(v_gpu)
-
+    v_gpu = CUDA.zeros(Float64, n_particles, 6)
+    bunch = Bunch(v_gpu)
+    track!(bunch, bitsring)
+    end
+end
 # Track the bunch through the ESR
 #@time begin
 #	track!(bunch, ring)
