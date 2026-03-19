740e8f53c6f2ed630816afbc68178467389368f0
Joseph P. Devlin <jpd285@cornell.edu>
2026-03-19 19:06:24 -0400

    Typo

--- test/BeamlinesExt/beamlines_ibs_test.jl
+++ test/BeamlinesExt/beamlines_ibs_test.jl
@@ -6,7 +6,7 @@ using Random
   # Here just check that they don't bug out
   drift = Drift(L = 2.0, 
   tracking_method = Yoshida(ibs_damping_on = true, ibs_fluctuations_on = true))
-  line = Beamline([drift], species_ref = Species("electron"), E_ref = -18e9)
+  line = Beamline([drift], species_ref = Species("electron"), E_ref = 18e9)
 
   v0 = randn(10,6)
   b0 = Bunch(copy(v0), species = line.species_ref, p_over_q_ref = line.p_over_q_ref)
