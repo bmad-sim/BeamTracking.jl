fe07d6aadb825d71088b99a4b7f9ed75bc9167ac
Joseph Devlin <jpd285@cornell.edu>
2026-07-29 13:44:29 -0400

    fix

--- src/kernels/quadrupole_kick.jl
+++ src/kernels/quadrupole_kick.jl
@@ -167,7 +167,7 @@ s: element length
 end 
 
 
-@makekernel fastgtpsa=true quadrupole_magnus6!(i, coords::Coords, k1, tilde_m, a, L)
+@makekernel fastgtpsa=true function quadrupole_magnus6!(i, coords::Coords, k1, tilde_m, a, L)
   v = coords.v
   q = coords.q
 
