bmad-sim/BeamTracking.jl

commit 28234d776421e2f365b7bb3c6e439be70e38296b

fix sagan cav

ext/BeamTrackingBeamlinesExt/unpack_bl.jl
@@ -315,7 +315,47 @@ function universal!(coords, tm::SaganCavity, ele, ramp_particle_energy_without_r
   isactive(rfparams) || error("SaganCavity Tracking through element $ele_name without RFParams is undefined")
 
   beta_gamma_ref = R_to_beta_gamma(bunch.species, bunch.p_over_q_ref)
-  kc = KernelChain(Val{6}(), RefState(bunch.t_ref, beta_gamma_ref))
+
+  # Compute information about reference coordinate system:
+  t_enter = bunch.t_ref
+  if p_over_q_ref isa TimeDependentParam
+    beta_gamma_enter = R_to_beta_gamma(bunch.species, p_over_q_ref(t_enter))
+  else
+    beta_gamma_enter = R_to_beta_gamma(bunch.species, p_over_q_ref)
+  end
+  g = isnothing(bendparams) ? (0,0) : reverse((bendparams.g_ref .* sincos(bendparams.tilt_ref)))
+  ds_step = (L == 0 || isactive(patchparams)) ? L : BeamTracking.find_steps(tm, L)[2]
+  # reference time change
+  if L != 0
+    species = bunch.species
+    p1_over_q_ref = p_over_q_ref
+    rf_omega = rf_omega_calc(rfparams, beamlineparams)
+    n_cells, L_active = rf_step_calc(tm.n_cells, tm.L_active, rf_omega, L)
+    L_outer = (L - L_active) / 2
+    E1_ref = R_to_E(species, p1_over_q_ref)
+    dE_ref = beamlineparams.dE_ref
+    E0_ref = E1_ref - dE_ref
+    dt_ref = L_outer/E_to_v(species, E0_ref) + L_outer/E_to_v(species, E1_ref)
+ 
+    if n_cells == 0
+      L_inner = L_active / 2
+      dt_ref += L_inner/E_to_v(species, E0_ref) + L_inner/E_to_v(species, E1_ref)
+    else
+      for i_step = 1:n_cells
+        E_now_ref = E0_ref + (i_step - 1/2) * dE_ref / n_cells
+        dt_ref += L_active / (n_cells * E_to_v(species, E_now_ref))
+      end
+    end
+    t_exit = dt_ref
+  else
+    t_exit = 0
+  end
+  if p_over_q_ref isa TimeDependentParam
+    beta_gamma_exit = R_to_beta_gamma(bunch.species, p_over_q_ref(t_exit))
+  else
+    beta_gamma_exit = R_to_beta_gamma(bunch.species, p_over_q_ref)
+  end
+  kc = KernelChain(Val{10}(), RefState(; t_enter, beta_gamma_enter, t_exit, beta_gamma_exit, L, g, ds_step))
 
   # Ramping
   if p_over_q_ref isa TimeDependentParam
@@ -370,34 +410,10 @@ function universal!(coords, tm::SaganCavity, ele, ramp_particle_energy_without_r
 
   # noinline necessary here for small binaries and faster execution
   @noinline launch!(coords, kc; kwargs...)
-
-  # reference time change
-  if L != 0
-    species = bunch.species
-    p1_over_q_ref = beamlineparams.beamline.p_over_q_ref
-    rf_omega = rf_omega_calc(rfparams, beamlineparams)
-    n_cells, L_active = rf_step_calc(tm.n_cells, tm.L_active, rf_omega, L)
-    L_outer = (L - L_active) / 2
-    E1_ref = R_to_E(species, p1_over_q_ref)
-    dE_ref = beamlineparams.dE_ref
-    E0_ref = E1_ref - dE_ref
-    dt_ref = L_outer/E_to_v(species, E0_ref) + L_outer/E_to_v(species, E1_ref)
- 
-    if n_cells == 0
-      L_inner = L_active / 2
-      dt_ref += L_inner/E_to_v(species, E0_ref) + L_inner/E_to_v(species, E1_ref)
-    else
-      for i_step = 1:n_cells
-        E_now_ref = E0_ref + (i_step - 1/2) * dE_ref / n_cells
-        dt_ref += L_active / (n_cells * E_to_v(species, E_now_ref))
-      end
-    end
-
-    bunch.t_ref += dt_ref
-  end
-
+  
+  bunch.t_ref = t_exit
+  
   return nothing
-
 end
 
 #---------------------------------------------------------------------------------------------------

ext/BeamTrackingBeamlinesExt/general_bl.jl
@@ -17,14 +17,24 @@
   if isactive(bendparams) && (bendparams.g_ref != 0 || bendparams.tilt_ref != 0)
     if entering
       mid_r, mid_q, st, ct = BeamTracking.coord_alignment_bend_mid(x_off, y_off, z_off, x_rot, y_rot, tilt, bendparams.g_ref, bendparams.tilt_ref, L)
-      return push(kc, make_kernel_call(BeamTracking.track_coord_bend_transform_at_s!, (mid_r, mid_q, st, ct, bendparams.g_ref, L, 0, Val{true}())))
+      kc = push(kc, make_kernel_call(BeamTracking.track_coord_bend_transform_at_s!, (mid_r, mid_q, st, ct, bendparams.g_ref, L, 0, Val{true}())))
+      return kc
     else
       mid_r, mid_q, st, ct = BeamTracking.coord_alignment_bend_mid(x_off, y_off, z_off, x_rot, y_rot, tilt, bendparams.g_ref, bendparams.tilt_ref, L)
       return push(kc, make_kernel_call(BeamTracking.track_coord_bend_transform_at_s!, (mid_r, mid_q, st, ct, bendparams.g_ref, L, L, Val{false}())))
     end
   else
     if entering
-      return push(kc, make_kernel_call(BeamTracking.track_alignment_straight_at_s!, (x_off, y_off, z_off, x_rot, y_rot, tilt, ele_orient, L, 0, Val{true}())))
+      kc = push(kc, make_kernel_call(BeamTracking.track_alignment_straight_at_s!, (x_off, y_off, z_off, x_rot, y_rot, tilt, ele_orient, L, 0, Val{true}())))
+      kc = push_transforms_out(kc, make_kernel_call(
+          BeamTracking.callback_alignment_straight_at_s!, (x_off, y_off, z_off, x_rot, y_rot, tilt, ele_orient, L, Val{false}())
+        )
+      )
+      kc = push_transforms_in(kc, make_kernel_call(
+          BeamTracking.callback_alignment_straight_at_s!, (x_off, y_off, z_off, x_rot, y_rot, tilt, ele_orient, L, Val{true}())
+        )
+      )
+      return kc
     else
       return push(kc, make_kernel_call(BeamTracking.track_alignment_straight_at_s!, (x_off, y_off, z_off, x_rot, y_rot, tilt, ele_orient, L, L, Val{false}())))
     end

src/kernels/alignment.jl
@@ -21,6 +21,14 @@
   end
 end
 
+# TRANSFORM FOR CALLBACKS:
+# note this doesn't have the s argument after L, instead using cur_s
+@inline function callback_alignment_straight_at_s!(
+  i, coords, cur_s, cur_t_ref, x_off, y_off, z_off, x_rot, y_rot, tilt, ele_orient, L, in
+  )
+  return track_alignment_straight_at_s!(i, coords, x_off, y_off, z_off, x_rot, y_rot, tilt, ele_orient, L, cur_s, in)
+end
+
 #---------------------------------------------------------------------------------------------------
 """
     function track_coord_transform!(i, coords::Coords, r, q)
@@ -51,4 +59,12 @@ end
     r_inv = .-quat_rotate(r, q_inv)
     track_coord_transform!(i, coords, r_inv, q_inv)
   end
+end
+
+# TRANSFORM FOR CALLBACKS:
+# note this doesn't have the s argument after L, instead using cur_s
+@inline function callback_coord_bend_transform_at_s!(
+  i, coords, cur_s, cur_t_ref, mid_r, mid_q, st, ct, g_ref, L, in,
+  )
+  return track_coord_bend_transform_at_s!(i, coords, mid_r, mid_q, st, ct, g_ref, L, cur_s, in)
 end

commit 8c302c6e9789d3ba17cd6f175744c5baeb542c1a

Fix bugs in SaganCavity universal!
- The reference time was reset instead of accumulated: `t_exit` was set
  to the transit time `dt_ref` (or 0 for L == 0) and then assigned to
  `bunch.t_ref`, a regression from 28234d7. It is now `t_enter + dt_ref`
  (or `t_enter`), as in the generic `universal!`.
- The BendParams check tested `patchparams` a second time, so BendParams
  were never rejected.
- Error messages interpolated the undefined `ele_name`, raising an
  UndefVarError instead of the intended error.
- Remove a stray `\` joining two checks with left division.

Add tests for the reference time with a nonzero initial t_ref, the error
messages, and do_not_use with a SaganCavity.

Co-Authored-By: Claude Opus 5 <noreply@anthropic.com>

ext/BeamTrackingBeamlinesExt/unpack_bl.jl
@@ -364,12 +364,12 @@ function universal!(coords, tm::SaganCavity, ele, ramp_particle_energy_without_r
   bendparams = isactive(bendparams, do_not_use) ? bendparams : nothing
   bmultipoleparams = isactive(bmultipoleparams, do_not_use) ? bmultipoleparams : nothing
 
-  !isactive(mapparams, do_not_use) || error("SaganCavity Tracking through element $ele_name with MapParams is undefined")
-  !isactive(patchparams, do_not_use) || error("SaganCavity Tracking through element $ele_name with PatchParams is undefined")
-  !isactive(patchparams, do_not_use) || error("SaganCavity Tracking through element $ele_name with BendParams is undefined")
-  !isactive(fourpotentialparams, do_not_use) || error("SaganCavity Tracking through element $ele_name with FourPotentialParams is undefined")\
-  !isactive(emultipoleparams, do_not_use) || error("SaganCavity Tracking through element $ele_name with EMultipoleParams is undefined")
-  isactive(rfparams, do_not_use) || error("SaganCavity Tracking through element $ele_name without RFParams is undefined")
+  !isactive(mapparams, do_not_use) || error("SaganCavity Tracking through element $(ele.name) with MapParams is undefined")
+  !isactive(patchparams, do_not_use) || error("SaganCavity Tracking through element $(ele.name) with PatchParams is undefined")
+  !isactive(bendparams, do_not_use) || error("SaganCavity Tracking through element $(ele.name) with BendParams is undefined")
+  !isactive(fourpotentialparams, do_not_use) || error("SaganCavity Tracking through element $(ele.name) with FourPotentialParams is undefined")
+  !isactive(emultipoleparams, do_not_use) || error("SaganCavity Tracking through element $(ele.name) with EMultipoleParams is undefined")
+  isactive(rfparams, do_not_use) || error("SaganCavity Tracking through element $(ele.name) without RFParams is undefined")
 
   beta_gamma_ref = R_to_beta_gamma(bunch.species, bunch.p_over_q_ref)
 
@@ -403,9 +403,9 @@ function universal!(coords, tm::SaganCavity, ele, ramp_particle_energy_without_r
         dt_ref += L_active / (n_cells * E_to_v(species, E_now_ref))
       end
     end
-    t_exit = dt_ref
+    t_exit = t_enter + dt_ref
   else
-    t_exit = 0
+    t_exit = t_enter
   end
   if p_over_q_ref isa TimeDependentParam
     beta_gamma_exit = R_to_beta_gamma(bunch.species, p_over_q_ref(t_exit))

test/BeamlinesExt/beamlines_sagan_cavity_test.jl
@@ -102,3 +102,47 @@ end
 
 # dE_ref
 
+
+@testset "SaganCavityUniversal" begin
+  # The reference time is accumulated through a SaganCavity (not reset)
+  t0 = 1e-6
+  ele = sc1
+  b1 = Bunch(deepcopy(v1), deepcopy(quat1), species=species, p_over_q_ref = BT.E_to_R(species, ele.E_ref-ele.dE_ref))
+  b1.t_ref = t0
+  track!(b1, ele)
+  @test b1.coords.v ≈ out1
+  @test b1.t_ref ≈ t0 + 6.671281911876738e-9
+
+  ele = sc3 # L = 0
+  b1 = Bunch(deepcopy(v1), deepcopy(quat1), species=species, p_over_q_ref = BT.E_to_R(species, ele.E_ref-ele.dE_ref))
+  b1.t_ref = t0
+  track!(b1, ele)
+  @test b1.coords.v ≈ out3
+  @test b1.t_ref ≈ t0
+
+  # Errors for parameter groups not supported by SaganCavity name the element
+  sc_bend = RFCavity(name = "sc_bend", L = 2.0, voltage = 0.2*E0, rf_frequency = 1e9, g_ref = 0.01,
+                     tracking_method = SaganCavity(n_cells = 2))
+  sc_patch = RFCavity(name = "sc_patch", L = 2.0, voltage = 0.2*E0, rf_frequency = 1e9, dx = 1e-3,
+                      tracking_method = SaganCavity(n_cells = 2))
+  for (ele, pg) in ((sc_bend, "BendParams"), (sc_patch, "PatchParams"))
+    bl = Beamline([ele], species_ref = species, E_ref = E0)
+    b1 = Bunch(deepcopy(v1), deepcopy(quat1), species = species)
+    @test_throws "SaganCavity Tracking through element $(ele.name) with $pg is undefined" track!(b1, bl)
+  end
+
+  # With the BendParams in do_not_use, the cavity is tracked as if it has no BendParams
+  sc_bend = RFCavity(L = 2.0, voltage = 0.2*E0, rf_frequency = 1e9, dE_ref = 0.1*E0, g_ref = 0.01,
+                     tracking_method = SaganCavity(n_cells = 2), do_not_use = [:BendParams])
+  sc_nobend = RFCavity(L = 2.0, voltage = 0.2*E0, rf_frequency = 1e9, dE_ref = 0.1*E0,
+                       tracking_method = SaganCavity(n_cells = 2))
+  bs = map((sc_bend, sc_nobend)) do sc
+    ele = Branch([Marker(E_ref = E0, species_ref = species), sc]).beamlines[2].line[1]
+    b = Bunch(deepcopy(v1), deepcopy(quat1), species=species, p_over_q_ref = BT.E_to_R(species, ele.E_ref-ele.dE_ref))
+    track!(b, ele)
+    return b
+  end
+  @test bs[1].coords.v ≈ bs[2].coords.v
+  @test bs[1].coords.q ≈ bs[2].coords.q
+  @test bs[1].t_ref ≈ bs[2].t_ref
+end